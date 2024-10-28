f3addcfd34449b99cffb803b7ec5a497ef78cd8d
MickeyLindgren <146181935+MickeyLindgren@users.noreply.github.com>
2024-10-28 13:39:44 +0100

    Create costumer_count.py

---
+++ VM/templates/costumer_count.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import sqlite3
+import paho.mqtt.subscribe as subscribe
+
+print("Subscribe MQTT script running!")
+
+def create_table():
+    try:
+        query = """CREATE TABLE IF NOT EXISTS costumer_count(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    count INTEGER NOT NULL, 
+                    date TEXT NOT NULL)"""
+        conn = sqlite3.connect("aktionshus.db")
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"sqlite3 error: {e}")
+        conn.rollback()
+    except Exception as e:
+        print(f"error: {e}")
+    finally:
+        conn.close()
+
+def insert_data(count):
+    try:
+        current_datetime = datetime.now()
+        formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        data = (count, formatted_datetime)
+        
+        query = """INSERT INTO costumer_count(count, date) VALUES (?, ?)"""
+        conn = sqlite3.connect("aktionshus.db")
+        cur = conn.cursor()
+        cur.execute(query, data)
+        conn.commit()
+        
+    except sqlite3.Error as e:
+        print(f"sqlite3 error: {e}")
+        conn.rollback()
+    except Exception as e:
+        print(f"error: {e}")
+    finally:
+        conn.close()
+
+create_table()
+
+def get_data(client, userdata, message):
+    print("Received message:")
+    print(f"Topic: {message.topic}")
+    print(f"Payload: {message.payload}")
+
+    try:
+        data = message.payload.decode('utf-8')
+        print(f"Decoded Data: {data}")
+
+        elements = data.split()
+        last_element = elements[-1]
+
+        count = int(last_element)
+        insert_data(count)
+
+    except UnicodeDecodeError as e:
+        print(f"Failed to decode message payload: {e}")
+    except ValueError as e:
+        print(f"Failed to convert last element to integer: {e}")
+
+subscribe.callback(get_data, "7c", hostname="74.235.100.12", userdata={"message_count": 0})
